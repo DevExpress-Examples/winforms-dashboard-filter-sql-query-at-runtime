2d7fd3d1697a90ee967de610498e01d439bb14e9
BrianDX <boris.zaitsev@devexpress.com>
2019-09-19 13:39:08 +0300

    update VB

---
+++ VB/Dashboard_CustomFilterExpression_Win/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports DevExpress.DataAccess
+Imports DevExpress.Data.Filtering
+
+Namespace Dashboard_CustomFilterExpression_Win
+	Partial Public Class Form1
+		Inherits DevExpress.XtraEditors.XtraForm
+
+		Public Sub New()
+			InitializeComponent()
+			dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
+		End Sub
+
+		Private Sub dashboardViewer1_CustomFilterExpression(ByVal sender As Object, ByVal e As CustomFilterExpressionEventArgs) Handles dashboardViewer1.CustomFilterExpression
+			If e.DataSourceName = "SQL Data Source 1" AndAlso e.TableName = "Invoices" Then
+				e.FilterExpression = New BinaryOperator("CustomerID","AROUT",BinaryOperatorType.Equal)
+			End If
+		End Sub
+	End Class
+End Namespace
